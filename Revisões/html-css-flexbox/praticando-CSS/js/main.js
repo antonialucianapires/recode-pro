d5f382866de9658ef0f4f3d95fda223c974af0c8
Antonia Luciana Pires <antonialucianapires@gmail.com>
2019-12-14 00:57:49 -0200

    Finalização da home RecodeNews e inclusão de exercício

---
+++ Revisões/html-css-flexbox/praticando-CSS/js/main.js
@@ -0,0 +1,9 @@
+//pop-up
+
+function abrirPopup() {
+    document.getElementById("popup").style.display = "block"
+}
+
+function fecharPopup() {
+    document.getElementById("popup").style.display = "none"
+}
