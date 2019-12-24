467f1fa2dd40c607b3fc97cd44a15abc903e0c69
Antonia Luciana Pires <antonialucianapires@gmail.com>
2019-12-24 18:42:35 -0200

    Exercício finalizado

--- Javascript/desafio-cadastro-de-livros/js/main.js
+++ Javascript/desafio-cadastro-de-livros/js/main.js
@@ -1,13 +1,13 @@
 function exibirReferencia() {
-    let nome = document.getElementById("myForm").elements[0].value;
-    let sobrenome  = document.getElementById("myForm").elements[1].value[0];
+    let nome = document.getElementById("myForm").elements[0].value[0];
+    let sobrenome  = document.getElementById("myForm").elements[1].value;
     let titulo  = document.getElementById("myForm").elements[2].value;
     let edicao  = document.getElementById("myForm").elements[3].value;
     let local = document.getElementById("myForm").elements[4].value;
     let editora = document.getElementById("myForm").elements[5].value;
     let ano = document.getElementById("myForm").elements[6].value;
 
-    document.getElementById("resultado").innerHTML = `Referência ABNT: ${sobrenome}, ${nome}. ${titulo}: ${edicao}. ${local}: ${editora}, ${ano}.`
-    
+    document.getElementById("resultado").innerHTML = `${sobrenome.toUpperCase()}, ${nome}. ${titulo.bold()}. Edição: ${edicao}. ${local}: ${editora}, ${ano}.`
 
+    document.getElementById("resultado").style.display = 'block';
 }
