1c4883c602dbe72f5093d542493355b6c5a3d6e0
Antonia Luciana Pires <antonialucianapires@gmail.com>
2020-02-03 17:39:51 -0300

    Introdução à Python

---
+++ python/primeiro-app/venv/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return '<h1> Aula de Python com Flask </h1>' \
+               '<p> Bom dia! </p>' \
+               '<p>Até mais</p>'
+
+    @app.route("/produtos")
+    def produtos():
+        return "<h1> Lista de produtos</h1>"
+
+    return app
